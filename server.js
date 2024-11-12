503165277464933fe8fb650f3731443a3ab58bc5
juancamilocorralesosvath <juancamilocorra@gmail.com>
2024-11-12 08:58:33 -0500

    added authentication functionality

--- server.js
+++ server.js
@@ -1,10 +1,57 @@
 const express = require('express')
 const app = express()
+// para encriptar las contraseñas de los usuarios, de esa manera aunque
+// alguien logre acceder a nuestra base de datos, no va a poder ver las contraseñas
+// es una libreria asincrona
+const bcrypt = require('bcrypt')
 
-const users = []
+app.use(express.json())
+
+const users = [{
+    name:'john doe'
+}]
 
 app.get('/users', (req, res) => {
     res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    try{
+        // entre mas grande el numero del salt mas tiempo va a tomar, pero es mas seguro
+        const salt = await bcrypt.genSalt()
+        // para hacer el hash, toma la contra del user y el salt que generamos
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        console.log("salt:"+salt)
+        console.log("hash:"+hashedPassword)
+
+        const user = {
+            name: req.body.name,
+            // bcrypt se encarga de guardar el salt dentro del hash
+            password: hashedPassword
+        }
+        users.push(user)
+        res.status(201).send()
+    }catch{
+        res.status(500).send()
+    }
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null){
+        return res.status(400).send('user does not exists')
+    }
+
+    try {
+        // usamos bcrypt para comparar
+        if (await bcrypt.compare(req.body.password, user.password)){
+            res.send('success!!!')
+        } else{
+            res.send('wrong username or password...')
+        }
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 app.listen(3000)
